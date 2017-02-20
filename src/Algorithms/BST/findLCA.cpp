cf90b8b4d01ae6827c18b439ad322d8b737a78a0
Shishir Mahajan <shishir.mahajan@redhat.com>
2017-02-19 21:12:00 -0500

    Recursive Lowest Common Ancestor (LCA) in a Binary Search Tree.
    Signed-off-by: Shishir Mahajan <shishir.mahajan@redhat.com>

--- src/Algorithms/BST/findLCA.cpp
+++ src/Algorithms/BST/findLCA.cpp
@@ -1,7 +1,7 @@
 // Iterative Lowest Common Ancestor (LCA) in a Binary Search Tree.
 // findLCA: Time Complexity: O(h). Space Complexity O(h) where h is the height of the BST.
 // findLCANew: Time Complexity: O(h). Space Complexity O(1).
-
+// findLCARecursive: Time Complexity: O(h). Space Complexity O(h)
 
 #include <iostream>
 #include <stack>
@@ -20,6 +20,7 @@ Node* createNode(Node*, int);
 void inorder(Node *);
 Node* findLCA(Node *,int, int);
 Node* findLCANew(Node *,int,int);
+Node* findLCARecursive(Node *, int, int);
 bool isExists(Node *,int);
 
 int main(){
@@ -40,7 +41,7 @@ cout<<"\nEnter n1\n";
 cin>>n1;
 cout<<"Enter n2\n";
 cin>>n2;
-Node *lca=findLCANew(root, n1, n2);
+Node *lca=findLCARecursive(root, n1, n2);
 if (!lca)
 {
 cout<<"LCA not found: Either n1 or n2 or both do not exist in the BST\n";
@@ -59,6 +60,18 @@ else if (n<root->data)
     return isExists(root->left, n);
 else
    return isExists(root->right, n);
+}
+
+Node* findLCARecursive(Node *root, int n1, int n2){
+if (!root)
+   return root;
+
+if (n1 < root->data && n2 < root->data)
+   return findLCARecursive(root->left, n1, n2);
+else if (n1 > root->data && n2 > root->data)
+   return findLCARecursive(root->right, n1, n2);
+
+return root;
 }
 
 Node* findLCANew(Node *root, int n1, int n2){
