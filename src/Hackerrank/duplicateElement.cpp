8ac2cd43a18b016a520bb710cee35ee86ae7fccd
Shishir Mahajan <shishir.mahajan@redhat.com>
2017-03-01 12:21:52 -0500

    Find duplicate element in O(log n).
    Signed-off-by: Shishir Mahajan <shishir.mahajan@redhat.com>

--- src/Hackerrank/duplicateElement.cpp
+++ src/Hackerrank/duplicateElement.cpp
@@ -19,7 +19,27 @@ prev=curr;
 return -1;
 }
 
-
+// Extension to solution 1. Can you do it O(log n).
+// HINT: Binary Search.
+// Time Complexity O(log n). Space Complexity O(1).
+int findDuplicate2(int a[], int size){
+int low=0;
+int high=size-1;
+int mid=0;
+while(low < high){
+	mid=low+(high-low)/2;
+	if (mid == a[mid]){
+		if (mid+1 == a[mid+1]+1)
+			break;
+		low=mid;
+	}else if (mid > a[mid]){
+		if (mid-1 == a[mid-1])
+			break;
+		high=mid;
+	}
+}
+return a[mid];
+}
 
 int main(){
 int a[]={0,1,2,3,4,5,5,6,7,8,9,10};
@@ -27,7 +47,7 @@ int size=sizeof(a)/sizeof(a[0]);
 cout<<"Original Array is:\n";
 for(int i=0;i<size;i++)
    cout<<a[i]<<" ";
-int result=findDuplicate1(a, size);
+int result=findDuplicate2(a, size);
 cout<<"\nDuplicate element is: "<<result<<endl;
 return 0;
 }
